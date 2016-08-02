a2d1be4ab0cb90e5fa16c17aef4cf93f8a499ad4
James Dempsey <james.e.dempsey@gmail.com>
2016-08-02 11:57:47 -0400

    Update ZPL segment according to new specs

--- lib/segments/zpl.rb
+++ lib/segments/zpl.rb
@@ -2,12 +2,10 @@
 require 'ruby-hl7'
 class HL7::Message::Segment::ZPL < HL7::Message::Segment
   weight 91
-  add_field :interface_id, idx: 2
-  add_field :provider_type, idx: 3
-  add_field :provider, idx: 4
-  add_field :referral_status, idx: 5
-  add_field :referred_on_date, idx: 6
-  add_field :referred_by_name, idx: 7
-  add_field :start_date, idx: 8
-  add_field :assigned_loc, idx: 9
+  add_field :interface_id, idx: 4
+  add_field :provider, idx: 5
+  add_field :service_type, idx: 6
+  add_field :provider_status, idx: 7
+  add_field :referral_status, idx: 8
+  add_field :start_date, idx: 16
 end
